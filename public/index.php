8ea12c3db53ab9426348af57acb33cd615e9c3c3
adding table <zangjiahou@hotmail.com>
2018-10-02 10:54:57 -0400

    repeating from class material

--- public/index.php
+++ public/index.php
@@ -6,5 +6,53 @@
  * Time: 8:51
  */
 
-echo "test";
-echo "wasdwasd";
+main::start();
+class main {
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        system::printPage($table);
+    }
+}
+class csv {
+    static public function getRecords() {
+        $make = 'Lambo';
+        $model = 'Bling Wagon';
+        $car = AutomobileFactory::create($make, $model);
+        $records[] = $car;
+        print_r($records);
+        return $records;
+    }
+}
+class html {
+    static public function generateTable($records) {
+        $table = $records;
+        return $table;
+    }
+}
+class system {
+    static public function printPage($page) {
+        echo $page;
+    }
+}
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
